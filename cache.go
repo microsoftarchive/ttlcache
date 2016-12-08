431cd5aa1dbd7a67caa60de2f9189fb625634efa
Ralph Caraveo <deckarep@gmail.com>
2016-12-07 16:07:47 -0800

    Refactor to use bytes instead of string, other stuff is style related.

--- cache.go
+++ cache.go
@@ -7,78 +7,79 @@ import (
 
 // Cache is a synchronised map of items that auto-expire once stale
 type Cache struct {
-	mutex sync.RWMutex
+	sync.RWMutex
 	ttl   time.Duration
 	items map[string]*Item
 }
 
+// NewCache is a helper to create instance of the Cache struct
+func New(duration time.Duration) *Cache {
+	cache := &Cache{
+		ttl:   duration,
+		items: make(map[string]*Item, 0),
+	}
+	cache.startEvictionTimer()
+	return cache
+}
+
 // Set is a thread-safe way to add new items to the map
-func (cache *Cache) Set(key string, data string) {
-	cache.mutex.Lock()
+func (cache *Cache) Set(key string, data []byte) {
+	cache.Lock()
+	defer cache.Unlock()
+
 	item := &Item{data: data}
 	item.touch(cache.ttl)
 	cache.items[key] = item
-	cache.mutex.Unlock()
 }
 
 // Get is a thread-safe way to lookup items
 // Every lookup, also touches the item, hence extending it's life
-func (cache *Cache) Get(key string) (data string, found bool) {
-	cache.mutex.Lock()
+func (cache *Cache) Get(key string) ([]byte, bool) {
+	cache.Lock()
+	defer cache.Unlock()
+
 	item, exists := cache.items[key]
 	if !exists || item.expired() {
-		data = ""
-		found = false
-	} else {
-		item.touch(cache.ttl)
-		data = item.data
-		found = true
+		return nil, false
 	}
-	cache.mutex.Unlock()
-	return
+
+	item.touch(cache.ttl)
+	return item.data, true
 }
 
 // Count returns the number of items in the cache
-// (helpful for tracking memory leaks)
 func (cache *Cache) Count() int {
-	cache.mutex.RLock()
+	cache.RLock()
+	defer cache.RUnlock()
+
 	count := len(cache.items)
-	cache.mutex.RUnlock()
 	return count
 }
 
-func (cache *Cache) cleanup() {
-	cache.mutex.Lock()
+func (cache *Cache) startEvictionTimer() {
+	duration := cache.ttl
+	if duration < time.Second {
+		duration = time.Second
+	}
+
+	ticker := time.Tick(duration)
+	go func() {
+		for {
+			select {
+			case <-ticker:
+				cache.evict()
+			}
+		}
+	}()
+}
+
+func (cache *Cache) evict() {
+	cache.Lock()
+	defer cache.Unlock()
+
 	for key, item := range cache.items {
 		if item.expired() {
 			delete(cache.items, key)
 		}
 	}
-	cache.mutex.Unlock()
-}
-
-func (cache *Cache) startCleanupTimer() {
-	duration := cache.ttl
-	if duration < time.Second {
-		duration = time.Second
-	}
-	ticker := time.Tick(duration)
-	go (func() {
-		for {
-			select {
-			case <-ticker:
-				cache.cleanup()
-			}
-		}
-	})()
-}
-
-// NewCache is a helper to create instance of the Cache struct
-func NewCache(duration time.Duration) *Cache {
-	cache := &Cache{
-		ttl:   duration,
-		items: map[string]*Item{},
-	}
-	cache.startCleanupTimer()
-	return cache
 }
